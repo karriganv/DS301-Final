1246ce8aef3eaf35a56505d3942844d969080732
Mitch-Aiken420 <99370030+Mitch-Aiken420@users.noreply.github.com>
2023-04-25 17:02:18 -0500

    Update DS301Final.R

--- DS301Final.R
+++ DS301Final.R
@@ -1,3 +1,17 @@
 library(tidyverse)
 
 Ellie Karrigan Megan
+
+df <- read.csv("Aemf1.csv")
+head(df)
+library(tidyverse)
+
+model <- lm(Price~., data = df)
+
+set.seed(1)
+
+sample <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.7,0.3))
+train  <- df[sample, ]
+test   <- df[!sample, ]
+dim(train)
+dim(test)
