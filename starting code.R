323e696b78bf473ad842413e4cd29569a96e41d1
ellieflynn <ellie.flynnsnider@gmail.com>
2023-04-26 21:40:54 -0500

    exploration

--- starting code.R
+++ starting code.R
@@ -1,6 +1,30 @@
 df <- read.csv("Aemf1.csv")
 head(df)
 library(tidyverse)
+library(ggplot2)
+
+#EXPLORATION
+
+str(df)
+
+dim(df)
+
+variable.names(df)
+
+sum(is.na(df))
+
+summary(df$Price)
+
+df$City <- as.factor(df$City)
+df$Day <- as.factor(df$Day)
+df$Room.Type <- as.factor(df$Room.Type)
+
+summary(df$City)
+
+ggplot(df, aes(x = City, fill = City)) + geom_histogram(stat = "count")
+
+
+##
 
 model <- lm(Price~., data = df)
 
