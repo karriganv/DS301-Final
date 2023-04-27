f5cd4aeebb0ee310111f4cec5e26bee19a4a02e3
MegGroat <98770323+MegGroat@users.noreply.github.com>
2023-04-26 23:04:46 -0500

    edits

--- DS301Final.R
+++ DS301Final.R
@@ -1,17 +1,18 @@
 library(tidyverse)
 
-Ellie Karrigan Megan
+# Ellie Karrigan Megan
 
-df <- read.csv("Aemf1.csv")
-head(df)
+airbnb = read.csv("https://raw.githubusercontent.com/karriganv/DS301-Final/main/Aemf1.csv")
+head(airbnb)
 library(tidyverse)
 
-model <- lm(Price~., data = df)
+model <- lm(Price~., data = airbnb)
 
 set.seed(1)
 
-sample <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.7,0.3))
-train  <- df[sample, ]
-test   <- df[!sample, ]
+sample <- sample(c(TRUE, FALSE), nrow(airbnb), replace=TRUE, prob=c(0.7,0.3))
+train  <- airbnb[sample, ]
+test   <- airbnb[!sample, ]
 dim(train)
 dim(test)
+
